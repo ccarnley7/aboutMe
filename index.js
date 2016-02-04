767d537c8fba0a171142b8a29e46dde6c7e9d069
Christian Carnley <ccarnley@simplifile.com>
2016-02-04 09:25:37 -0700

    got the gulp inject css and js to work

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT;
+var port = process.env.PORT || 9931;
 
 app.use(express.static('public'));
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send("I hear you");
+    res.render('index');
 });
 
 app.listen(port, function (err) {
